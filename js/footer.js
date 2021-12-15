723f4b7a897344ac3ecd54bd092b5cab614ad1d0
Moisés Arão Rodrigues Magalhães <63241484+moises-magalhaes@users.noreply.github.com>
2021-12-15 12:40:10 +0100

    Initial commit

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+export function footer() {
+	return `
+  <footer class="footer">
+    <div class="container">
+      <div class="container__icons"><i class="container__icons__icon fa fa-vimeo"></i> <i class="container__icons__icon fa fa-youtube-play"></i></i></div>
+      <div class="container__text"><p class="container__text__paragraph">hello@yay.com</p></div>
+      <div class="container__text">Copyright 2021</div>
+  </div>
+  </footer>
+  `;
+}
